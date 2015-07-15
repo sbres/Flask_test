263eade287aeeaa2ff5682f455669451a15f1206
Stephane Bres <stephanejrbres@gmail.com>
2015-07-15 17:38:43 +0200

    for testing prupose

---
+++ solo_app.py
@@ -0,0 +1,27 @@
+import logging
+import logging.handlers
+import json
+
+logger = logging.getLogger(__name__)
+logger.setLevel(logging.DEBUG)
+
+# Formatter
+formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+
+# Add Formatter to Handler
+#handler.setFormatter(formatter)
+
+# add Handler to Loggers
+#logger.addHandler(handler)
+
+from flask import Flask, request
+application = Flask(__name__)
+
+@application.route('/')
+def hello_world():
+    return 'Coucou ^.^'
+
+if __name__ == '__main__':
+    application.debug = True
+    application.run(host='0.0.0.0', port=8000)
+    #application.run()
